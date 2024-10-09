89278279d7d63f0527823af55faf8b0e39d34cc6
Yuriy2648 <149948840+Yuriy2648@users.noreply.github.com>
2024-10-09 15:41:56 +0300

    Add files via upload

---
+++ code.js
@@ -0,0 +1,14 @@
+let point = 0;
+
+
+function addPoint(x) {
+    point+=x;
+    document.getElementById('score').innerHTML = point;
+}
+
+let point2 = 0;
+
+function addPoint2(y) {
+    point2+=y;
+    document.getElementById('score2').innerHTML = point2;
+}
